4dbfe6362f305e30de2979a4baa44db65535f3ed
od <od@sweettracker.co.kr>
2024-11-22 15:12:08 +0900

    feat: productAgent (추후 작업할 out adapter 선언)

--- product_agent/src/main/java/com/product_agent/product/adapter/in/update_product_hit_count/UpdateProductHitCountScheduler.java
+++ product_agent/src/main/java/com/product_agent/product/adapter/in/update_product_hit_count/UpdateProductHitCountScheduler.java
@@ -1,5 +1,17 @@
 package com.product_agent.product.adapter.in.update_product_hit_count;
 
 public class UpdateProductHitCountScheduler {
-
+    /*
+        조회수 : MongoDB 적재 -> batch update (RDB, ElasticSearch)
+            실시간 정확성이 중요하지 않기 때문에 batch update 로 처리
+            쓰기 성능을 고려하여 mongo 사용
+        구매수 : kafka 를 이용한 비동기 업데이트 (ElasticSearch)
+            실시간 반영이 중요함. 보낼 때 최종 구매수를 보내야함
+            업데이트 실패 시 재처리를 위한 DLQ (Dead Letter Queue) 설계 필요
+        리뷰수 : kafka 를 이용한 비동기 업데이트 (RDB, ElasticSearch)
+            실시간 반영이 중요함. 보낼 때 최종 리뷰수를 보내야함
+            업데이트 실패 시 재처리를 위한 DLQ (Dead Letter Queue) 설계 필요
+        총점수 : 업데이트 대상을 몽고에 적재 -> batch update (RDB, ElasticSearch)
+            대상 : 조회수, 구매수, 리뷰수에 변경 내역이 있는 사용자
+     */
 }
--- product_agent/src/main/java/com/product_agent/product/adapter/in/update_product_sales_count/UpdateProductSalesCountConsumer.java
+++ product_agent/src/main/java/com/product_agent/product/adapter/in/update_product_sales_count/UpdateProductSalesCountConsumer.java
@@ -1,5 +1,5 @@
 package com.product_agent.product.adapter.in.update_product_sales_count;
 
-public class UpdateProductSalesCountScheduler {
+public class UpdateProductSalesCountConsumer {
 
 }
