fcb5cf4057efacbb6f19608f7a3780249ab68885
od <od@sweettracker.co.kr>
2024-12-30 18:00:31 +0900

    test: product (누락된 UpdateProductQuantityServiceTest 추가)

--- product/src/test/java/com/product/product/application/service/update_product_quantity/UpdateProductQuantityServiceTest.java
+++ product/src/test/java/com/product/product/application/service/update_product_quantity/UpdateProductQuantityServiceTest.java
@@ -10,6 +10,7 @@ import static org.mockito.Mockito.verify;
 import com.product.IntegrationTestSupport;
 import com.product.global.exception.CustomAuthorizationException;
 import com.product.global.exception.CustomBusinessException;
+import com.product.global.exception.CustomNotFoundException;
 import com.product.global.exception.ErrorCode;
 import com.product.product.application.port.in.command.UpdateProductQuantityCommand;
 import com.product.product.application.port.out.DeleteProductPort;
@@ -20,6 +21,7 @@ import com.product.product.domain.Category;
 import com.product.product.domain.Product;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -27,6 +29,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 class UpdateProductQuantityServiceTest extends IntegrationTestSupport {
 
@@ -204,6 +207,82 @@ class UpdateProductQuantityServiceTest extends IntegrationTestSupport {
             // then
             assertThat(result.result()).isEqualTo("Y");
             assertThat(updatedProduct.getQuantity()).isEqualTo(12);
+        }
+
+        @Test
+        @DisplayName("[error] 조회된 상품 정보가 없으면 예외를 응답한다.")
+        void error3() {
+            // given
+            UpdateProductQuantityCommand command = UpdateProductQuantityCommand.builder()
+                .authorization(createAccessToken("ROLE_SELLER"))
+                .productId(12345L)
+                .productCount(1)
+                .isPurchased(false)
+                .build();
+
+            // when
+            CustomNotFoundException exception = assertThrows(CustomNotFoundException.class,
+                () -> service.updateProductQuantity(command));
+
+            // then
+            assertThat(exception.getErrorCode()).isEqualTo(ErrorCode.DoesNotExist_PROUCT_INFO);
+        }
+
+        @Test
+        @DisplayName("[success] 다수의 사용자가 동시에 상품수량을 수정하려고 할 때 분산락이 잘 동작하는지 확인한다.")
+        void success3() throws InterruptedException {
+            // given
+            Product product = registerProductPort.register(Product.builder()
+                .productId(snowflakeGenerator.nextId())
+                .productName("아이폰 12")
+                .sellerId(1L)
+                .sellerEmail("test@gmail.com")
+                .productImg("상품 이미지")
+                .productOption(List.of("옵션1", "옵션2"))
+                .description("애플의 신제품 아이폰 12 입니다.")
+                .price(10000)
+                .quantity(100)
+                .reviewCount(0)
+                .reviewScore(0)
+                .totalScore(0)
+                .hitCount(0)
+                .embeddingYn("N")
+                .category(Category.ELECTRONICS)
+                .regDate("20210801")
+                .regDateTime(LocalDateTime.now())
+                .build());
+            UpdateProductQuantityCommand command = UpdateProductQuantityCommand.builder()
+                .authorization(createAccessToken("ROLE_CUSTOMER"))
+                .productId(product.getProductId())
+                .productCount(1)
+                .isPurchased(true)
+                .build();
+
+            int count = 50;
+            ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+            executor.setCorePoolSize(50);
+            executor.setMaxPoolSize(50);
+            executor.initialize();
+            CountDownLatch latch = new CountDownLatch(count);
+
+            //  when
+            for (int i = 0; i < count; i++) {
+                executor.execute(() -> {
+                    try {
+                        service.updateProductQuantity(command);
+                    } catch (Exception e) {
+                        e.getStackTrace();
+                    } finally {
+                        latch.countDown();
+                    }
+                });
+            }
+            latch.await();
+            Product updatedProduct = findProductPort.findById(product.getProductId());
+            Thread.sleep(1000);
+            
+            // then
+            assertThat(updatedProduct.getQuantity()).isEqualTo(50);
         }
     }
 }
