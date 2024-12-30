bf147071e89dec62583424ffeb221058959e6fa8
od <od@sweettracker.co.kr>
2024-12-30 18:01:04 +0900

    feat: product (상품수량 업데이트시 구매 유무 필수값 제거)

--- product/src/main/java/com/product/product/adapter/in/update_product_quantity/UpdateProductQuantityRequest.java
+++ product/src/main/java/com/product/product/adapter/in/update_product_quantity/UpdateProductQuantityRequest.java
@@ -1,10 +1,8 @@
 package com.product.product.adapter.in.update_product_quantity;
 
-import com.product.global.validation.groups.ValidationGroups.NotBlankGroups;
 import com.product.global.validation.groups.ValidationGroups.SizeGroups;
-import com.product.product.application.port.in.command.UpdateProductSalesCommand;
+import com.product.product.application.port.in.command.UpdateProductQuantityCommand;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,7 +14,6 @@ class UpdateProductQuantityRequest {
     @Min(value = 1, message = "상품 수량은 1 이상 이어야 합니다", groups = SizeGroups.class)
     private int productCount;
 
-    @NotNull(message = "구매 여부는 필수값 입니다", groups = NotBlankGroups.class)
     private Boolean isPurchased;
 
     @Builder
@@ -25,10 +22,10 @@ class UpdateProductQuantityRequest {
         this.isPurchased = isPurchased;
     }
 
-    UpdateProductSalesCommand toCommand(Long productId, String authorization) {
-        return UpdateProductSalesCommand.builder()
+    UpdateProductQuantityCommand toCommand(Long productId, String authorization) {
+        return UpdateProductQuantityCommand.builder()
             .productCount(productCount)
-            .isPurchased(isPurchased)
+            .isPurchased(isPurchased != null && isPurchased)
             .productId(productId)
             .authorization(authorization)
             .build();
