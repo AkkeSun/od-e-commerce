27ebf3ac862cb33eac48de5f48c8564666b7cb0f
od <od@sweettracker.co.kr>
2025-01-06 16:25:54 +0900

    refactor: product (ElasticSearchRepository 로 쿼리 수정)

--- product/src/main/java/com/product/product/adapter/out/persistence/elasticSearch/ProductEsPersistenceAdapter.java
+++ product/src/main/java/com/product/product/adapter/out/persistence/elasticSearch/ProductEsPersistenceAdapter.java
@@ -1,6 +1,7 @@
 package com.product.product.adapter.out.persistence.elasticSearch;
 
 import com.product.product.application.port.in.command.FindProductListCommand;
+import com.product.product.application.port.out.DeleteProductEsPort;
 import com.product.product.application.port.out.FindProductEsPort;
 import com.product.product.application.port.out.RegisterProductEsPort;
 import com.product.product.application.port.out.ResetProductEsIndex;
@@ -12,53 +13,47 @@ import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.SearchHits;
-import org.springframework.data.elasticsearch.core.query.Criteria;
-import org.springframework.data.elasticsearch.core.query.CriteriaQuery;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 class ProductEsPersistenceAdapter implements RegisterProductEsPort, FindProductEsPort,
-    ResetProductEsIndex {
+    ResetProductEsIndex, DeleteProductEsPort {
 
     @Value("${service-constant.product.response-page-size}")
     private int responsePageSize;
     private final ProductEsDocumentMapper productMapper;
-    private final ElasticsearchOperations elasticsearchOperations;
+    private final ProductEsRepository productEsRepository;
 
     @Override
     public Product register(Product product) {
-        ProductEsDocument savedDocument = elasticsearchOperations
-            .save(productMapper.toDocument(product));
+        ProductEsDocument savedDocument = productEsRepository.save(
+            productMapper.toDocument(product));
         return productMapper.toDomain(savedDocument);
     }
 
     @Override
     public LinkedHashSet<Product> findByKeyword(FindProductListCommand command) {
-        CriteriaQuery query;
         if (command.category().equals(Category.TOTAL)) {
-            query = new CriteriaQuery(Criteria.where("productName").matchesAll(command.keyword())
-                .or(Criteria.where("description").matchesAll(command.keyword())));
-        } else {
-            query = new CriteriaQuery(Criteria.where("category").is(command.category().name())
-                .and(Criteria.where("productName").matchesAll(command.keyword())
-                    .or(Criteria.where("description").matchesAll(command.keyword()))));
+            return productEsRepository.findByKeyword(command.keyword(),
+                    makePageRequest(command.page(), responsePageSize,
+                        command.sortType())).stream()
+                .map(productMapper::toDomain)
+                .collect(Collectors.toCollection(LinkedHashSet::new));
         }
 
-        query.setPageable(makePageRequest(command.page(), responsePageSize, command.sortType()));
-        SearchHits<ProductEsDocument> searchHits = elasticsearchOperations
-            .search(query, ProductEsDocument.class);
-
-        return searchHits.stream()
-            .map(hits -> productMapper.toDomain(hits.getContent()))
+        return productEsRepository.findByKeywordAndCategory(command.category().name(),
+                command.keyword(),
+                makePageRequest(command.page(), responsePageSize, command.sortType()))
+            .stream()
+            .map(productMapper::toDomain)
             .collect(Collectors.toCollection(LinkedHashSet::new));
     }
-    
-    private PageRequest makePageRequest(int page, int size, ProductSortType sortType) {
+
+    private Pageable makePageRequest(int page, int size, ProductSortType sortType) {
         return switch (sortType) {
             case LOWEST_PRICE -> PageRequest.of(page, size, Sort.by(Direction.ASC, "price"));
             case HIGHEST_PRICE -> PageRequest.of(page, size, Sort.by(Direction.DESC, "price"));
@@ -71,7 +66,11 @@ class ProductEsPersistenceAdapter implements RegisterProductEsPort, FindProductE
 
     @Override // for test
     public void resetIndex() {
-        elasticsearchOperations.indexOps(ProductEsDocument.class).delete();
-        elasticsearchOperations.indexOps(ProductEsDocument.class).create();
+        productEsRepository.deleteAll();
+    }
+
+    @Override
+    public void deleteById(Long productId) {
+        productEsRepository.deleteById(productId);
     }
 }
---
+++ product/src/main/java/com/product/product/adapter/out/persistence/elasticSearch/ProductEsRepository.java
@@ -0,0 +1,59 @@
+package com.product.product.adapter.out.persistence.elasticSearch;
+
+import java.util.List;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.annotations.Query;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+interface ProductEsRepository extends ElasticsearchRepository<ProductEsDocument, Long> {
+
+    @Query("""
+            {
+                "bool": {
+                    "should": [
+                        {
+                            "match": {
+                                "productName": "?0"
+                            }
+                        },
+                        {
+                            "match": {
+                                "description": "?0"
+                            }
+                        }
+                    ],
+                    "minimum_should_match": 1
+                }
+            }
+        """)
+    List<ProductEsDocument> findByKeyword(String keyword, Pageable pageable);
+
+    @Query("""
+            {
+                "bool": {
+                    "must": [
+                        {
+                            "term": {
+                                "category.keyword": "?0"
+                            }
+                        }
+                    ],
+                    "should": [
+                        {
+                            "match": {
+                                "productName": "?1"
+                            }
+                        },
+                        {
+                            "match": {
+                                "description": "?1"
+                            }
+                        }
+                    ],
+                    "minimum_should_match": 1
+                }
+            }
+        """)
+    List<ProductEsDocument> findByKeywordAndCategory(String category, String keyword,
+        Pageable pageable);
+}
